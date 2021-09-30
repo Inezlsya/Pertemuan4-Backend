14dd67d68b63da78d08f80c5e56fdc3a2268fcef
Inezlsya <89917337+Inezlsya@users.noreply.github.com>
2021-10-01 00:18:48 +0700

    Add files via upload

---
+++ beranda.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Orestoran</title>
+<link rel="stylesheet" type="text/css" href="style6.css">
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css">
+</head>
+<body bgcolor="skyblue">
+  <div class="dropdwn">
+    <nav>
+      <label>ORESTORAN</label>
+      <ul>
+        <li><a href="beranda.php">Home</a></li>
+        <li><a href="transaksi.php">Entri Transaksi</a></li>
+        <li><a href="order.php">Entri Order</a></li>
+      </ul>
+    </nav>
+  </div>
+  <section>
+  <div class="body">
+        <div class="umkm1">
+          <center><h1>SELAMAT DATANG
+Di Sistem Pelayanan Restaurant Cepat Saji.
+Semoga Hari Anda Menyenangkan.</h1></center>
+  <div class="login100-form-avatar">
+            <center><img src="template/masuk/images/avatar-02.jpg" alt="AVATAR" width="400px" height="400px"><center>
+          </div>
+          
+        </div>
+   </div>
+</section>
+</body>
+</html>
